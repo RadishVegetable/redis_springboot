47d72ad825366724e8cf870c26a4b9a0d178f397
RadishVegetable <1075225782@qq.com>
2023-03-10 09:50:17 +0800

    first commit

---
+++ src/main/java/com/shrank/redis_springboot/controller/RedisTestController.java
@@ -0,0 +1,27 @@
+package com.shrank.redis_springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/redisTest")
+public class RedisTestController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping
+    public String testRedis() {
+        redisTemplate.opsForValue().set("name","lucy");
+        String name = (String) redisTemplate.opsForValue().get("name");
+
+        return name;
+
+    }
+}
